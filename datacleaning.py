465f7e70960087a543fc4c2c3f8b07261688317b
E2023 <59032869+licds@users.noreply.github.com>
2022-04-13 16:06:12 -0400

    try weight and frequency

--- datacleaning.py
+++ datacleaning.py
@@ -4,12 +4,22 @@ import string
 import pandas as pd
 from nltk.stem.wordnet import WordNetLemmatizer
 from collections import defaultdict
-#from textblob import Textblob
+from textblob import TextBlob
 #from textblob.np_extractors import ConllExtractor
 
 def main():
     df = initdf("Data/test.csv")
     dict = first_convert(df)
+    
+
+    state = True
+    while state != False:
+        sentence = input("Enter a sentence: ")
+        if sentence == "f":
+            state = False
+            break
+        testimonial = TextBlob(sentence)
+        print(testimonial.sentiment)
     
 def initdf(csv_file):
     f=open(csv_file)
@@ -42,6 +52,35 @@ def first_convert(df):
         for word in temp:
             dict1[word].append(emotion[row])
     return dict1
+
+"""
+def weight(dict):
+    weighted = defaultdict(list)
+    for key in dict.keys():
+        weighted[key] = sum(dict[key])/len(dict[key])
+    return weighted
+
+def frequency(dict):
+    freq = defaultdict(list)
+    for key in dict.keys():
+        freq[key] = nltk.FreqDist(dict[key]).max()
+    return freq
+
+def emotion_sentiment(emotion):
+    emotioncp = round(emotion)
+    if emotioncp == 0:
+        emotion_type = 'Sadness'
+    elif emotioncp == 1:
+        emotion_type = 'Joy'
+    elif emotioncp == 2:
+        emotion_type = 'Love'
+    elif emotioncp == 3:
+        emotion_type = 'Anger'
+    else:
+        emotion_type = 'Fear'
+    print("Emotion detected:", emotion_type, "     ", "The actual weight is: ", emotioncp)
+    return
+"""
 
 if __name__ == "__main__":
     main()
