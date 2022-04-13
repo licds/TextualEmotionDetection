2c1e74d409e707ea626ab993dd9fbf2855f1de39
E2023 <59032869+licds@users.noreply.github.com>
2022-04-13 01:38:38 -0400

    df and tokenize initial completion

--- datacleaning.py
+++ datacleaning.py
@@ -7,27 +7,27 @@ from textblob import TextBlob
 from textblob.np_extractors import ConllExtractor
 
 def main():
-    f = open("Data/test.csv")
+    f=open("Data/test.csv")
 
     rows=list(csv.reader(f))
-
+    rows.pop(0)
     for row in rows:
-        row[0]=tokenize(row[0])
+        row[0]=list(tokenize(row[0]))
     
-    df = todataframe(rows)
-    print(df)
-    
+    df = pd.DataFrame(rows, columns=['Text', 'Emotion'])
+    df['Emotion'] = df['Emotion'].astype(int)
+
 def tokenize(text):
-    tokens = nltk.word_tokenize(text)
+    #blob = TextBlob(text, np_extractor=ConllExtractor()).noun_phrases
+    #print(blob)
+    tokens=nltk.word_tokenize(text)
+    new=[]
     for word in tokens:
         word=word.lower()
         word=WordNetLemmatizer().lemmatize(word)
-        if (word in string.punctuation) and (word in nltk.corpus.stopwords.words("english")) :
-            tokens.remove(word)
-    return tokens
+        if word not in nltk.corpus.stopwords.words("english"):
+            new.append(word)
+    return new
 
-def todataframe(rawdf):
-    df = pd.DataFrame(rawdf, columns =["Text","Emotion"])
-    return df
 if __name__ == "__main__":
     main()
