b25c80cfc895b182a27ac0ea4f1f44e5e3869bbd
E2023 <59032869+licds@users.noreply.github.com>
2022-04-12 22:03:20 -0400

    tokenize improved

--- datacleaning.py
+++ datacleaning.py
@@ -3,26 +3,28 @@ import nltk
 import string
 import pandas as pd
 from nltk.stem.wordnet import WordNetLemmatizer
+from textblob import TextBlob
+from textblob.np_extractors import ConllExtractor
 
 def main():
     f = open("Data/test.csv")
 
     rows=list(csv.reader(f))
-    
+
     for row in rows:
         row[0]=tokenize(row[0])
     
     df = todataframe(rows)
+    print(df)
     
-def tokenize(document):
-    l=[]
-    tokens = nltk.word_tokenize(document)
+def tokenize(text):
+    tokens = nltk.word_tokenize(text)
     for word in tokens:
         word=word.lower()
         word=WordNetLemmatizer().lemmatize(word)
-        if (word not in string.punctuation) and (word not in nltk.corpus.stopwords.words("english")) :
-            l.append(word)
-    return l
+        if (word in string.punctuation) and (word in nltk.corpus.stopwords.words("english")) :
+            tokens.remove(word)
+    return tokens
 
 def todataframe(rawdf):
     df = pd.DataFrame(rawdf, columns =["Text","Emotion"])
